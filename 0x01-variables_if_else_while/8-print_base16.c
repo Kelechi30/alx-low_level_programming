da5b51283978d0d1ccb4bbb77e573f253f6ef80a
ogonna <kelechichukwuma@gmail.com>
2022-04-09 15:41:52 -0700

    c codes

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Print all the digits of base 16 in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+char base_i6;
+
+for (base_16 = 48; base_16 <= 57; base_16++)
+{putchar(base_16);
+}
+
+for (base_16 = 97; base_16 <= 102; base_16++)
+{
+putchar(base_16);
+}
+
+putchar('\n');
+
+return (0);
+}
