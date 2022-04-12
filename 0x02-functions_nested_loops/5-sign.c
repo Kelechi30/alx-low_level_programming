ed659418a39d30da7ea2ddf0f2a34c664b2dfc02
ogonna <kelechichukwuma@gmail.com>
2022-04-12 15:53:55 -0700

    c codes

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+
+/**
+ * print_sign - check numers
+ * @n : number to check
+ * Return:0 or 1
+ */
+
+int print_sign(int n)
+{
+int test;
+
+if (n > 0)
+{
+	test = 1;
+	_putchar('+');
+}
+else if (n == 0)
+{
+	test = 0;
+	_putchar('-');
+}
+
+return (test);
+}
