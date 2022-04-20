a6eab9b6d80610740466c38b7d3f135ceb71ea58
ogonna <kelechichukwuma@gmail.com>
2022-04-20 13:54:06 -0700

    c codes

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,15 +8,15 @@
 
 void print_rev(char *s)
 {
-int i;
+		int i = 0;
 
-i = 0;
-while (s[i] != '\0')
-{
-i++;
-}
+		while (s[i])
+				i++;
 
-for (i = i - 1 ; i >= 0; i--)
-{
-_putchar (s[i]);
+		while (i--)
+		{
+				_putchar(s[i]);
+		}
+
+		_putchar('\n');
 }
