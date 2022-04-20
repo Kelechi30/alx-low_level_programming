4cf410647e471612122db276c9fae7d57103d993
ogonna <kelechichukwuma@gmail.com>
2022-04-20 12:46:55 -0700

    c codes

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *_strlen -length of a string
- *@s: A pointer to an int that will be changed/updated
+ * _strlen -find length of a string
+ * @s: A pointer to an int that will be changed/updated
  *
 
  *Return: void that means our answer is correct
