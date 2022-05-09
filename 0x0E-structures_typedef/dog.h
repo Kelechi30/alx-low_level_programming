036208fa50c80d23caeabbc2a77036ad2a82fb43
kelechi <kelechichukwuma930@gmail.com>
2022-05-09 22:19:36 +0100

    c codes

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+* struct dog - this structure defines the dog
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+*
+* Description: This is a structure in the header file that sets the
+* parameters of the dog.
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
